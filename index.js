aad122724b025c5d740cc2127ad90ebc6a9ba116
monnaung <monnaung.dev@gmail.com>
2023-02-28 13:22:17 +0630

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,15 @@
-// index.js
-// where your node app starts
 
-// init project
 var express = require('express');
 var app = express();
 
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
-
-// http://expressjs.com/en/starter/static-files.html
+app.use(cors({optionsSuccessStatus: 200})); 
 app.use(express.static('public'));
-
-// http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
-});
-
-
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
-});
+app.get('/api', (req, res) => {
+  const currentDate = new Date().toUTCString()
+  const currentUnix = Date.parse(currentDate)
+  res.json({ unix: currentUnix, utc: currentDate })
+})
 
 app.get('/api/:date?', (req, res) => {
   const dateString = req.params.date
@@ -45,15 +32,9 @@ app.get('/api/:date?', (req, res) => {
     res.json({ unix: unixTimestamp, utc: utcDate })
   }
  
-   app.get('/api', (req, res) => {
-     const currentDate = new Date().toUTCString()
-     const currentUnix = Date.parse(currentDate)
-     res.json({ unix: currentUnix, utc: currentDate })
-   })
  })
 
 
-// listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
